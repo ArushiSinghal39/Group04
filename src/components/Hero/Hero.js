a3fc7e32bf49bf083dde1f18aa49927e0d07787d
21042 <singhaln514@gmail.com>
2022-04-29 10:47:02 +0530

    gcfhbkj

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,39 +3,110 @@ import Image from "../Images/Images"
 
 const Hero = () => {
   return (
-    <div className="container col-xxl-8 px-4 py-5">
-      <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div className="col-10 col-sm-8 col-lg-6">
-          <Image
-            className="d-block mx-lg-auto img-fluid shadow"
-            style={{ borderRadius: "10px" }}
-            filename="1.png"
-            alt="hero"
-          />
+    <div>
+      <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
         </div>
-        <div className="col-lg-6">
-          <h1 className="fat-text lh-1 mb-3">
-            BuyNearby!! Your Local Online Store
-          </h1>
-          <p className="lead">
-            Many of you continue to shop at your local stores even today and are
-            still greeted with that nostalgic, homely smile. We share an idyllic
-            bond of trust and convenience with your local
-            shopkeepers.Undoubtedly, these Stores form an essential part of our
-            Indian fabric.you can now conveniently buy your daily essentials
-            online from your local trustworthy store and enjoy more efficient
-            ordering, delivery, and payment services that the shopkeeper is
-            providing by embracing digital.
-          </p>
-          <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button
-              type="button"
-              className="btn btn-primary shadow px-4 me-md-2"
-            >
-              Read More
-            </button>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <Image
+              className="d-block mx-lg-auto img-fluid"
+              filename={"c1.jpg"}
+              alt="1"
+            />
+            <div class="container">
+              <div class="carousel-caption text-start">
+                <h1 className="text-warning">
+                  Order Online.Schedule Deliveries. Pay Online on Delivery{" "}
+                </h1>
+                <p>
+                  <h5>
+                    Conveniently order anything - branded or unbranded -from
+                    your nearest store .Pay be cash,card or net banking on
+                    delivery
+                  </h5>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <Image
+              className="d-block mx-lg-auto img-fluid"
+              filename={"c2.jpg"}
+              alt="1"
+            />
+            <div class="container">
+              <div class="carousel-caption">
+                <h1 className="text-warning">
+                  "Your Nearby General Store is Now online!"
+                </h1>
+
+                <p>
+                  <h5>
+                    your Local trustworthy stores even closer to you . just a
+                    tap away on your smartphone
+                  </h5>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <Image
+              className="d-block mx-lg-auto img-fluid"
+              filename={"c3.jpg"}
+              alt="1"
+            />
+            <div class="container">
+              <div class="carousel-caption text-end">
+                <h1 className="text-warning">"Shop Locally"</h1>
+                <p>
+                  <h5>
+                    Connect to premium brands and in-stock products at your
+                    favourite local shops
+                  </h5>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   )
